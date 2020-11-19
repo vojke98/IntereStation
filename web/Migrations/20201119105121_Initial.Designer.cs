32463a576f46a3d7fc1d3f2236403fd00f552893
DEJAN VOJINOVIC <DEJANVOJINOVIC@yahoo.com>
2020-11-19 11:56:09 +0100

    Refreshed migrations.

--- web/Migrations/20201119105121_Initial.Designer.cs
+++ web/Migrations/20201119105121_Initial.Designer.cs
@@ -10,7 +10,7 @@ using web.Data;
 namespace web.Migrations
 {
     [DbContext(typeof(ISDBContext))]
-    [Migration("20201118215911_Initial")]
+    [Migration("20201119105121_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
