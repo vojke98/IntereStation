13213a4f3275eff1b00c95a7d23051fee4c91ee1
DEJAN VOJINOVIC <DEJANVOJINOVIC@yahoo.com>
2021-01-06 21:52:23 +0100

    Api

--- web/Startup.cs
+++ web/Startup.cs
@@ -78,7 +78,7 @@ namespace web
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Interestation API V1");
             });
         }
     }
